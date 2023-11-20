9b18bad5d908f0385a59f45497404fb8c14b1e7d
lucile bessac <lucilebessac@free.fr>
2023-11-20 11:32:16 +0100

    devoirs html 20-11

--- Miniprojet/Programmes/miniprojet.sh
+++ Miniprojet/Programmes/miniprojet.sh
@@ -14,15 +14,15 @@ then
 	exit
 fi
 
-echo "<html><head></head><body><table>" > ../Tableaux/miniprojet.html
-echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th></tr>" >> ../Tableaux/miniprojet.html
+echo "<html><head><link rel="stylesheet" href="../Css/main.css"></head><body><div class ="container"><table id="table_ru">" > ../Html/tableau_fr.html
+echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th></tr>" >> ../Html/tableau_fr.html
 lineno=1
 while read -r URL
 do
 	response=$(curl -s -I -L -w "%{http_code}" -o /dev/null $URL)
 	encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
-	echo "<tr><td>$lineno</td><td>$URL</td><td>$response</td><td>$encoding</td></tr>" >> ../Tableaux/miniprojet.html
+	echo "<tr><td>$lineno</td><td>$URL</td><td>$response</td><td>$encoding</td></tr>" >> ../Html/tableau_fr.html
 	lineno=$(expr $lineno + 1)
 done < "$URLS"
 
-echo "</table></body></html>" >> ../Tableaux/miniprojet.html
+echo "</table></div></body></html>" >> ../Html/tableau_fr.html
